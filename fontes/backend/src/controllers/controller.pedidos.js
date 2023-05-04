22fdbb78218047b89382bcd7b081b262972982cb
Sara Roseane <80297582+sararoseane@users.noreply.github.com>
2023-05-04 17:52:48 -0300

    tes

--- fontes/backend/src/controllers/controller.pedidos.js
+++ fontes/backend/src/controllers/controller.pedidos.js
@@ -5,11 +5,11 @@ import db from "../config/database.js";
 const controllerPedidos = Router();
 
 //listando dados de um pedido
-controllerPedidos.get("/pedidos/:id_pedido", function(request, response){
-    
-    let ssql = "select * from pedido where id_pedido = ?" 
-    db.query(ssql,[request.params.id_pedido], function(err, result){
-        if (err){
+controllerPedidos.get("/pedidos/:id_pedido", function (request, response) {
+
+    let ssql = "select * from pedido where id_pedido = ?"
+    db.query(ssql, [request.params.id_pedido], function (err, result) {
+        if (err) {
             return response.status(500).send(err);
         } else {
             return response.status(200).json(result[0]);
